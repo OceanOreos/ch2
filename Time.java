3fe700a316a38910a9fae7b6c8d726f149fd5bb1
Will Zhu <67773442+OceanOreos@users.noreply.github.com>
2026-09-14 14:50:36 -0700

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,14 @@
+public class Time{
+	
+	public static void main (String[] args) {
+		
+		int hour;
+	    hour = 14;
+	    int minutes;
+	    minutes = 40;
+	    
+	    System.out.println("This is the seconds since midnight: " + (hour * 3600 + minutes * 60) + ".");
+	    System.out.print("It is currently 2:40 pm" + ".");
+	    
+	}
+}
---
+++ date.java
@@ -0,0 +1,15 @@
+public class date{
+	public static void main (String[] args) {
+		
+		int day;
+		day = 14;
+	    String month;
+	    month = "September";
+	    int year;
+	    year = 2026;
+	    
+	    System.out.println("Today is " + month + " " + day + " of " + year);
+	    
+	   }
+}
+	    
