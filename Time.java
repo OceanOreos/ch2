3f2510e661d430d1971f3fd934810b67a95d72f1
Will Zhu <67773442+OceanOreos@users.noreply.github.com>
2026-09-14 14:54:39 -0700

    Add files via upload

--- Time.java
+++ Time.java
@@ -2,11 +2,13 @@ public class Time{
 	
 	public static void main (String[] args) {
 		
+		// Declaring and assigning variables 
 		int hour;
 	    hour = 14;
 	    int minutes;
 	    minutes = 40;
 	    
+	    // Concatenate
 	    System.out.println("This is the seconds since midnight: " + (hour * 3600 + minutes * 60) + ".");
 	    System.out.print("It is currently 2:40 pm" + ".");
 	    
