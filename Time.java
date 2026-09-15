1663d5aa3d901d6a004bfa269f25db4a13308c01
Will Zhu <67773442+OceanOreos@users.noreply.github.com>
2026-09-15 14:51:59 -0700

    Add files via upload

--- Time.java
+++ Time.java
@@ -3,19 +3,25 @@ public class Time{
 	public static void main (String[] args) {
 		
 		// Declaring and assigning variables 
-		int hour;
-	    hour = 14;
-	    int minutes;
-	    minutes = 26;
-	    int second;
-	    second = 21;
+		int hour = 14;
+	    int minutes = 41;
+	    int second = 12;
 	    
 	    // Concatenate
+	    System.out.println("It is currently 2:22 pm" + ".");
 	    System.out.println("This is the seconds since midnight: " + (hour * 3600 + minutes * 60) + ".");
-	    System.out.println("It is currently 2:26 pm" + ".");
 	    
 	    // Percentage
 	    System.out.print("This is the percentage of the day that has passed: ");
 	    System.out.println(((hour*60)+minutes)*100/1440 + "%");
+	    
+	    // New 
+	    minutes = 28;
+	    second = 60;
+	    
+	    // Q6
+	    System.out.println("It is currently 2:50 pm" + ".");
+	    System.out.print("Elapsed Time: " + (minutes*second) + " " + "seconds");
+	    
 	}
 }
